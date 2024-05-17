ebe304e0a7ed76d8f52fd4bc3e3af561d1811634
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-05-17 12:52:22 +0500

    created the selected component & added the logic for the seletected countries to show

---
+++ Select_Fav/src/components/ConDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ConDetail() {
+  return (
+    <div className='module'>
+      <div className="module_heading"></div>
+      .module_name
+      
+    </div>
+  )
+}
+
+export default ConDetail
